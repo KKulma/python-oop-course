d1a239eabf233c5c9f6f8524b3d0223e0aac4ac5
Kasia <katarzyna.kulma@gmail.com>
2021-04-05 21:32:14 +0100

    add ch8 assignment

--- 08-methods.py
+++ 08-methods.py
@@ -124,3 +124,23 @@ pizza = Pizza()
 pizza.add_topping("mushrooms").add_topping("olives").add_topping(
     "chicken"
 ).display_toppings()
+
+
+## assignments
+class Circle:
+
+    pi = 3.1416
+
+    def __init__(self, radius, color):
+        self.radius = radius
+        self.color = color
+
+    # Add the method below this line
+    def find_area(self):
+        return Circle.pi * (self.radius) ** 2
+
+
+blue_circle = Circle(15, "Blue")
+
+# Call the method with blue_circle and assign the result to the variable final_area
+blue_circle.find_area()
